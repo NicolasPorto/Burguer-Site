7a2f4ec1bc3156ec67cb7a4939bf86c22f7bc0b5
Nicolas Porto <nicolasporto468@gmail.com>
2023-01-05 11:16:09 -0300

    Projeto Burguer

---
+++ src/views/PedidosView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="main-container">
+    <h1>Gerenciar pedidos: </h1>
+    <Dashboard />
+  </div>
+</template>
+
+<script>
+import Dashboard from "../components/Dashboard"
+
+export default {
+  name: "PedidosView",
+
+  components:{Dashboard}
+}
+</script>
